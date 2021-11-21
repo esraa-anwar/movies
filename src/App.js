77ee685909146d3104c8408e91087c1bcd2639c9
esraa-anwar <62084184+esraa-anwar@users.noreply.github.com>
2021-11-21 17:51:58 +0200

    fix error

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 import Movies from "./component/Movies"
 const FEATURED_API = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
-const IMG_API = "https://image.tmdb.org/t/p/w1280";
 
 const SEARCH_API = "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query="
 
